6c9b308e1cfdc169703e7f4fdc6cd91a7b436ee6
ktzchen <70239415+ktzchen@users.noreply.github.com>
2020-11-02 11:35:35 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -86,8 +86,6 @@ def generate_comment():
 #connect to reddit 
 reddit = praw.Reddit('bot')
 
-#Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:81.0) Gecko/20100101 Firefox/81.0
-
 print(reddit.user.me())
 
 # connect to the debate thread
